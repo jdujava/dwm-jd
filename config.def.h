b88b6c19ce5e77fc4aa618cc505bf807baaef953
Jonas Dujava <jonas.dujava@gmail.com>
2022-07-29 21:24:03 +0200

    rounded corners in bar

--- config.def.h
+++ config.def.h
@@ -16,13 +16,15 @@ static const int user_bh            = 4;        /* 2 is the default spacing arou
 static const char *fonts[]          = { "monospace:size=9:weight=heavy", "Font Awesome 6 Free Solid:size=9", "Font Awesome 6 Free Regular:size=9", "Material Icons:pixelsize=15:antialias=true", "Material Design Icons:pixelsize=15:antialias=true" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
+static const char col_gray2shade[]  = "#292929";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static const char col_cyanshade[]   = "#173542";
+static const char *colors[][ColLast]      = {
+	/*               fg         bg         bg_shade        border   */
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2shade, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyanshade,  col_cyan  },
 };
 
 /* tagging */
