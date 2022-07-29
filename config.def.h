daea1a57bff080d9d430a5595ca44fed5db3ead8
Jonas Dujava <jonas.dujava@gmail.com>
2022-07-29 19:26:22 +0200

    status2d + systray

--- config.def.h
+++ config.def.h
@@ -3,6 +3,11 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 15;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 3;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=9:weight=heavy", "Font Awesome 6 Free Solid:size=9", "Font Awesome 6 Free Regular:size=9", "Material Icons:pixelsize=15:antialias=true", "Material Design Icons:pixelsize=15:antialias=true" };
