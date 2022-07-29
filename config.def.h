e1e5188ee60bbbf43e403879074ad80ab5262701
Jonas Dujava <jonas.dujava@gmail.com>
2022-07-29 19:46:05 +0200

    swallow

--- config.def.h
+++ config.def.h
@@ -3,6 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 15;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 3;   /* systray spacing */
@@ -31,9 +32,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     iscentered   isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           -1 },
+	/* class     instance  title           tags mask    iscentered   isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    NULL,     NULL,           0,           0,           1,          0,           0,        -1 },
+	{ "Firefox", NULL,     NULL,           1 << 8,      0,           0,          0,          -1,        -1 },
+	{ "St",      NULL,     NULL,           0,           0,           0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,           0,           0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
--- config.mk
+++ config.mk
@@ -19,11 +19,15 @@ FREETYPELIBS = -lfontconfig -lXft
 FREETYPEINC = /usr/include/freetype2
 # OpenBSD (uncomment)
 #FREETYPEINC = ${X11INC}/freetype2
+#KVMLIB = -lkvm
 #MANPREFIX = ${PREFIX}/man
+
+# This is needed for the swallow patch
+XCBLIBS = -lX11-xcb -lxcb -lxcb-res
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
-LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
+LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} ${XCBLIBS} ${KVMLIB}
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
