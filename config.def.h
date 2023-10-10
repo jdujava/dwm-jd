3f60f107b24fa9f05fa38007f1cf19f0f9516447
Jonas Dujava <jonas.dujava@gmail.com>
2023-10-10 15:18:02 +0200

    fix drawing of statusbar for multi-monitor setups

--- config.def.h
+++ config.def.h
@@ -22,7 +22,7 @@ static const char *fonts[]          = {
 };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_gray2shade[]  = "#292929";
+static const char col_gray2shade[]  = "#222222";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
